931cc8ef3ca4fbe0dfcfd5a1ec9dc2d1fecc8d8e
Ranojay Sen <osgranojay@gmail.com>
2019-08-01 19:12:20 +0530

    update

--- PythonCode/cleanup.bat
+++ PythonCode/cleanup.bat
@@ -4,5 +4,6 @@ attrib *.opensdf -H
 
 FOR /f %%a IN ( 'dir /s /b *.*ipynb_checkpoints' ) Do del /S /Q  %%a
 FOR /f %%a IN ( 'dir /s /b *.*ipynb_checkpoints' ) Do rd /S /Q %%a
+FOR /f %%a IN ( 'dir /s /b *__pycache__*' ) Do rd /S /Q %%a
 
 pause
