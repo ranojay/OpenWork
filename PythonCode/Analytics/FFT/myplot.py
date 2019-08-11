51b0d538a811e0dc646e3051e05b3ee039c65bb0
Ranojay Sen <osgranojay@gmail.com>
2019-08-11 12:15:47 +0530

    updates

--- PythonCode/Analytics/FFT/myplot.py
+++ PythonCode/Analytics/FFT/myplot.py
@@ -1,25 +1,31 @@
 
-import matplotlib as mpl
 
 import matplotlib.pyplot as plt
 
 class MyPlot:
   def __init__(self):
-    self.plot = plt
-    self.sp = 121
+    self.fig = plt.figure(figsize=(20, 16))
+    self.pltpos = 0
     
 
-  def plot(self,points):
-    self.plot.plot(points)
-    print(points)
-    self.plot.show()
+  def plot(self,points,label=""):
+    self.pltpos += 1
+    self.ax = self.fig.add_subplot(3,2,self.pltpos)
+    self.ax.plot(points)
+    plt.xlabel(label)
+    self.fig.show()
 
-  def crossplot(self,xvars,yvars):
-    #self.plot.subplot(self.sp)
-    self.sp += 1
-    self.plot.plot(xvars,yvars)
-    self.plot.show()
+  def crossplot(self,xvars,yvars,label):
+    self.pltpos += 1
+    self.ax = self.fig.add_subplot(3,2,self.pltpos)
+    self.ax.plot(xvars,yvars)
+    plt.xlabel(label)
+    self.fig.show()
 
-  def histogram(self,points,bins):
-    self.plot.hist(points,bins)
-    self.plot.show()
+  def histogram(self,points,bins,label):
+    self.pltpos += 1
+    self.ax = self.fig.add_subplot(3,2,self.pltpos)
+    self.ax.hist(points,bins)
+    plt.xlabel(label)
+    #print(points)
+    self.fig.show()
