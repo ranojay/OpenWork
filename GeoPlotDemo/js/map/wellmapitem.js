6cab5a0b486ad7ec06638007acaf434fc02e43a5
Ranojay Sen <osgranojay@gmail.com>
2021-10-20 11:07:57 +0530

    some improvements

--- GeoPlotDemo/js/map/wellmapitem.js
+++ GeoPlotDemo/js/map/wellmapitem.js
@@ -7,15 +7,9 @@ class WellMapItem extends MapItem
 
     createShape()
     {
-        var myStyle =
-        {
-            color: 'red',
-            weight: 1
-        };
-
         var topcoords = [];
         topcoords.push(this.vertices[0]);
-        var latlong = this.getLatLongFromCoords(this.vertices, SG.zone);
+        var latlong = this.getLatLongFromCoords(topcoords, SG.zone);
         var marker = L.marker(latlong[0]);
         marker.bindPopup(this.item.name)
         return marker;
