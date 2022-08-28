7117983af3c82833a3a47e075f2e2b67c7cfaabd
ranojay <osgranojayu@gmail.com>
2022-08-28 20:41:43 +0530

    test with moc data

--- Angular/DemoApp/src/app/app.component.ts
+++ Angular/DemoApp/src/app/app.component.ts
@@ -11,5 +11,24 @@ export class AppComponent {
   title = 'DemoApp';
   constructor(private dataBase: DataBaseService) {}
 
-  sticks: SticksData[] = this.dataBase.getData();
+  //sticksIput: SticksData[] = this.dataBase.getData();
+
+  ticksIput: SticksData[] =  [
+    { id: 2002, place:"Ger", geometry: "" },
+    { id: 2003, place:"Ger", geometry: "" },
+    { id: 2004, place:"Ger", geometry: "" },
+    { id: 2005, place:"Ger", geometry: "" },
+    { id: 2006, place:"Ger", geometry: "" },
+    { id: 2007, place:"Ger", geometry: "" },
+    { id: 2008, place:"Ger", geometry: "" },
+    { id: 2009, place:"Ger", geometry: "" },
+    { id: 2010, place:"Ger", geometry: "" },
+    { id: 2011, place:"Ger", geometry: "" },
+    { id: 2012, place:"Ger", geometry: "" },
+    { id: 2013, place:"Ger", geometry: "" },
+    { id: 2014, place:"Ger", geometry: "" },
+    { id: 2015, place:"Ger", geometry: "" },
+    { id: 2016, place:"Ger", geometry: "" },
+    { id: 2017, place:"Ger", geometry: "" }
+  ];
 }
